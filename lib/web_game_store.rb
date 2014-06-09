5c016fe282ea57d259fc19d7c4ba4e105a042424
LNA <LAllen@8thLight.com>
2014-06-09 15:52:31 -0500

    Adds web game store

---
+++ lib/web_game_store.rb
@@ -0,0 +1,36 @@
+require 'ai'
+require 'board'
+require 'game'
+require 'settings'
+require 'game_rules'
+class WebGameStore
+  @@games = []
+
+  def self.games
+    return @@games
+  end
+
+  def self.games
+    @@games
+  end
+
+  def self.store(game)
+    @@games << self
+  end
+
+  def self.game_rules
+    @game_rules = GameRules.new
+  end
+
+  def self.board
+    @board = Board.new
+  end
+
+  def self.ai
+    @ai = AI.new(GameRules.new)
+  end
+
+  def self.settings(params = {})
+    @settings = Settings.new(params)
+  end
+end
