765d33b8c2b8d377dc3e39cd96c167ec9c71a230
LNA <LAllen@8thLight.com>
2014-06-09 10:42:27 -0500

    Adds game piece selection JScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,27 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+  $('#player_one_piece').on ('change', function(event){
+    var $this = $(this);
+    if (($this).val() == "X") {
+      $('#player_two_piece').val("O");
+    }
+    else{
+      $('#player_two_piece').val("X");
+    }
+  });
+});
+
+$(function() {
+  $('#player_two_piece').on ('change', function(event){
+    var $this = $(this);
+    if (($this).val() == "X") {
+      $('#player_one_piece').val("O");
+    }
+    else{
+      $('#player_one_piece').val("X");
+    }
+  });
+});
