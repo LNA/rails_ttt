d57bcdeb53a709e7bf6d37b5c2db2e764e8f6eb0
LNA <LAllen@8thLight.com>
2014-06-09 10:34:28 -0500

    Generates Games Controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+  def welcome
+  end
+
+  def auto_refresh_board
+  end
+
+  def board
+  end
+
+  def game_over
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'games/welcome'
+
+  get 'games/auto_refresh_board'
+
+  get 'games/board'
+
+  get 'games/game_over'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
