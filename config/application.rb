88cc34e66b6ef4828a6311a069f2bb6c46d1c652
LNA <LAllen@8thLight.com>
2014-06-09 15:00:47 -0500

    Accesses business logic from lib

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Ttt
   class Application < Rails::Application
+    # Autoload lib/ folder including all subdirectories
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
