40a6ea112377c5044ee0d5817f581bf2efa8c209
LNA <LAllen@8thLight.com>
2014-06-20 10:33:05 -0500

    Removes JSON functionality

--- spec/lib/interactor_spec.rb
+++ spec/lib/interactor_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
+
 describe Interactor do
   let (:test_interactor) { Interactor.new}
-  let (:test_string)     { "[\"X\",null,null,
-                              null,null,null,
-                              null,null,null]" }
+  let (:test_string)     { "[nil, nil, nil, nil, nil, nil, nil, nil, nil]" }
+
   it "converts the board from a string to a ruby object" do
-    expect(test_interactor.build_from(test_string)).to eq(["X", nil, nil, nil, nil, nil, nil, nil, nil])
+    expect(test_interactor.build_from(test_string)).
+           to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 end
