9b10aa98047dd5ab4511c6842f41791cee3c9571
LNA <LAllen@8thLight.com>
2014-06-20 10:30:56 -0500

    Removes settings

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,11 @@
 class Game
   attr_accessor :ai,
                 :board,
-                :game_rules,
-                :settings
+                :game_rules
 
-  def initialize(ai, board, game_rules, settings)
-    @ai         = ai
-    @board      = board
-    @game_rules = game_rules
-    @settings   = settings
+  def initialize(ai, board, game_rules)
+    @ai               = ai
+    @board            = board
+    @game_rules       = game_rules
   end
 end
