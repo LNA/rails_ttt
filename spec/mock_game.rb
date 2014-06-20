99a228cb8a34c7d032ef60ce456dd32262c937bd
LNA <LAllen@8thLight.com>
2014-06-20 10:33:28 -0500

    Removes settings, adds player factory

--- spec/mock_game.rb
+++ spec/mock_game.rb
@@ -2,12 +2,12 @@ class MockGame
   attr_accessor :ai, 
                 :board,
                 :game_rules, 
-                :settings
+                :player_factory
 
-  def initialize(ai, board, game_rules, settings)
+  def initialize(ai, board, game_rules, player_factory)
     @ai         = ai
     @board      = board
     @game_rules = game_rules
-    @settings   = settings
+    @player_factory   = player_factory
   end
 end
