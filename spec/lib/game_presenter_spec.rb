8b9f234b33cd07b55de3cc6e32ff917abcfb50db
LNA <LAllen@8thLight.com>
2014-06-20 10:32:47 -0500

    Swaps object specs for Mocks

--- spec/lib/game_presenter_spec.rb
+++ spec/lib/game_presenter_spec.rb
@@ -1,24 +1,16 @@
 require 'spec_helper'
 
 describe GamePresenter do
-  let (:test_player)     {Player.new('X', 'human')}
-  let (:board)           {Board.new}
-  let (:game_rules)      {GameRules.new}
-  let (:ai)              {AI.new(game_rules)}
-  let (:player_one)      { Player.new('X', "human")}
-  let (:player_two)      { Player.new('O', "ai")}
-  let (:params)          { {:current_player => player_one, :next_player => player_two} }
-  let (:settings)        {Settings.new( params)}
-  let (:game)            {Game.new(ai, board, game_rules, settings)}
-  let (:test_presenter)  {GamePresenter.new(game)}
+  let (:mock_game_rules)       { MockGameRules.new}
+  let (:mock_ai)               { MockAI.new}
+  let (:board)                 { Board.new}
+  let (:mock_players)          { MockPlayers.new}
+  let (:mock_game)             { MockGame.new(mock_ai, board, mock_game_rules, mock_players)}
+  let (:test_presenter)        { GamePresenter.new(mock_game)}
 
 
   it "converts the board to a string" do 
     expect(test_presenter.build_board). 
           to eq ("[nil, nil, nil, nil, nil, nil, nil, nil, nil]")
-  end
-
-  it "converts the current player to a string" do 
-    expect(test_presenter.build_current_player). to eq ("X")
   end
 end
