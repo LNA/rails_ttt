a49ec49b384b915296fda420708cdb5dc1dae6ca
LNA <LAllen@8thLight.com>
2014-06-20 14:35:36 -0500

    Adds functionality for next player

--- lib/players.rb
+++ lib/players.rb
@@ -1,16 +1,27 @@
 class Players
-  attr_accessor :mark, :params, :type,
+  attr_accessor :current_player_mark, :current_player_type,
+                :current_player, :params,
                 :player_one, :player_two
 
   def initialize(params = {}, player_one, player_two)
-    @params     = params
-    @player_one = player_one
-    @player_two = player_two
+    @params              = params
+    @player_one          = player_one
+    @player_two          = player_two
+    @current_player_mark = params["player_one_mark"]
+    @current_player_type = params["player_one_type"]
   end
 
   def create
     create_player_one
     create_player_two
+  end
+
+  def next_player_mark
+    @current_player_mark == @player_one.mark ? @player_two.mark : @player_one.mark
+  end
+
+  def next_player_type
+    @current_player_type == @player_one.type ? @player_two.type : @player_one.type
   end
 
 private
--- spec/lib/players_spec.rb
+++ spec/lib/players_spec.rb
@@ -10,17 +10,25 @@ describe Players do
   let (:player_two)    {Player.new}
   let (:test_players)  {Players.new(params, player_one, player_two)}
 
-  it "creates both player types" do 
+  before (:each) do 
     test_players.create
+  end
 
+  it "creates both player types" do 
     expect(test_players.player_one.type).to eq('Human')
     expect(test_players.player_two.type).to eq('AI')
   end
 
   it "creates both player marks" do 
-    test_players.create
-
     expect(test_players.player_one.mark).to eq('X')
     expect(test_players.player_two.mark).to eq('O')
+  end
+
+  it "returns the mark of the next player" do   
+    expect(test_players.next_player_mark).to eq('O')
+  end
+
+  it "returns the type for the next player" do 
+    expect(test_players.next_player_type).to eq('AI')
   end
 end
