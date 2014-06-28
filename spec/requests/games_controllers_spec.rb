e0670f7777eb8010f69ebe3aee9cde5596b90a0c
LNA <LAllen@8thLight.com>
2014-06-27 20:18:12 -0500

    Adds basic integration specs

--- spec/requests/games_controllers_spec.rb
+++ spec/requests/games_controllers_spec.rb
@@ -1,12 +1,58 @@
 require 'spec_helper'
 
 RSpec.describe "GamesControllers", :type => :request do
-  describe "POST /games_controllers" do
-    it "works! (now write some real specs)" do
-      # post 'new_game'
-      # (:game).should_receive(:new)
-      # (:game_presenter).should_receive(:new).with(:type_of_game)
-      # response.should be okay # will tell if view is broken/doesn't existx
+
+  describe "GET /games" do
+    it "returns the correct response status" do
+      get 'games'
+      expect(response.status).to eq(200) 
+    end
+
+    it "displays the content of the hompage" do
+      get 'games'
+
+      expect(response.body).to include("Start")
+    end
+  end
+
+  describe "POST /games" do
+   xit "returns the correct response status" do
+      post 'games'
+      expect(response.status).to eq(200) 
+    end
+  end
+
+  describe "POST /new_game" do
+    it "returns the correct response status" do
+      post 'new_game'
+      expect(response.status).to eq(200) 
+    end
+
+    it "displays the content of the hompage" do
+      post 'new_game'
+
+      expect(response.body).to include("Player 1:")
+    end
+  end
+
+  describe "POST /move" do
+    xit "returns the correct response status" do
+      post 'move'
+      expect(response.status).to eq(200) 
+    end
+
+    xit "displays the content of the hompage" do
+      post 'move'
+
+      expect(response.body).to include("Start")
+    end
+  end
+
+  describe "GET /ai_move" do
+    xit "returns the correct response status" do
+      get 'ai_move'
+
+      expect(response.status).to eq(200) 
     end
   end
 end
