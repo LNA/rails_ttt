8bd3c96fc926494877bd8bcc0099bb1f3e02fbf3
LNA <LAllen@8thLight.com>
2014-07-02 18:35:28 -0500

    Removes params

--- lib/players.rb
+++ lib/players.rb
@@ -3,12 +3,15 @@ class Players
                 :current_player, :params,
                 :player_one, :player_two
 
-  def initialize(params = {}, player_one, player_two)
-    @params              = params
+  def initialize(player_one_mark, player_one_type, player_two_mark, player_two_type, player_one, player_two)
+    @player_one_mark     = player_one_mark
+    @player_one_type     = player_one_type
+    @player_two_mark     = player_two_mark
+    @player_two_type     = player_two_type
+    @current_player_mark = player_one_mark
+    @current_player_type = player_one_type
     @player_one          = player_one
     @player_two          = player_two
-    @current_player_mark = params[:current_player_mark]
-    @current_player_type = params[:current_player_type]
   end
 
   def create
@@ -24,20 +27,20 @@ class Players
     @current_player_type == @player_one.type ? @player_two.type : @player_one.type
   end
 
-  def update
-    params[:current_player_mark]  = params[:next_player_mark]
-    params[:current_player_type]  = params[:next_player_type]
-  end
+  # def update
+  #   params[:current_player_mark]  = params[:next_player_mark]
+  #   params[:current_player_type]  = params[:next_player_type]
+  # end
 
 private
 
   def create_player_one
-    @player_one.mark = params["player_one_mark"]
-    @player_one.type = params["player_one_type"]
+    @player_one.mark = @player_one_mark
+    @player_one.type = @player_one_type
   end
 
    def create_player_two
-    @player_two.mark = params["player_two_mark"]
-    @player_two.type = params["player_two_type"]
+    @player_two.mark = @player_two_mark
+    @player_two.type = @player_two_type
   end
 end
