f6f74cd8613aeefc161b6b7eae3f1444dbc921cd
LNA <LAllen@8thLight.com>
2014-07-02 16:31:38 -0500

    Wraps logic

--- lib/process_move.rb
+++ lib/process_move.rb
@@ -12,15 +12,24 @@ class ProcessMove
     if params[:current_player_type] == HUMAN 
       @game.board.fill(params[:square].to_i, params[:current_player_mark]) 
     else
+      process_order_of_players
       ai_move
     end
   end
 
-  def ai_move #should return move.  Doing too much.
-    if @game.board.spaces.count(nil).even? #wrap in board, exposes too much information.  This object shouldn't know about boards details
-      params[:current_player_mark] = params[:player_two_mark]
-    end
+  def ai_move 
     best_move = @game.ai.find_best_move(@game.board, params[:current_player_mark], params[:next_player_mark])
     @game.board.fill(best_move,params[:current_player_mark])
+  end
+
+private
+  def ai_is_second_player?
+    @game.board.spaces.count(nil).even?
+  end
+
+  def process_order_of_players
+    if ai_is_second_player? 
+      params[:current_player_mark] = params[:player_two_mark]
+    end
   end
 end
