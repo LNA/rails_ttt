69800ea59b7e6f2b439c5a464871bfcfe99833d9
LNA <LAllen@8thLight.com>
2014-07-11 19:58:54 -0500

    Removes adapters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,30 +1,41 @@
 class GamesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
+  AI = "AI"
+
   def create 
-    @game = WebGameStore.game(params)
+    @game = GameWrapper.game(params)
     @player_presenter = PlayerPresenter.new(@game)
-    board_adapter = BoardAdapter.new(self, @player_presenter.current_player_type)  
-    board_adapter.render_board 
+    if @player_presenter.current_player == AI 
+      render_auto_refresh_board
+    else
+      render_human_board
+    end
   end
 
   def update  
-    @game = WebGameStore.updated_game(params)
+    @game = GameWrapper.updated_game(params)
     @player_presenter = PlayerPresenter.new(@game)
     @game.board = StringToObjectProcessor.new.build_from(params[:board])
     MovePresenter.present_move(@game, params[:square].to_i, @player_presenter) 
     board_wrapper = BoardWrapper.new(@game)
     params[:board] = board_wrapper.convert_board_to_string
-    update_adapter = UpdateAdapter.new(self, @game) 
-    update_adapter.check_for_winner(params)
+    if @game.game_rules.game_over?(@game.board)
+      render_game_over
+    else
+      update_game(params)
+    end
   end
 
   def update_game(params) 
-    @game = WebGameStore.updated_game(params)
+    @game = GameWrapper.updated_game(params)
     @game.board = StringToObjectProcessor.new.build_from(params[:board])
     @player_presenter = PlayerPresenter.new(@game)
-    board_adapter = BoardAdapter.new(self, @player_presenter.current_player_type) 
-    board_adapter.render_board
+    if @player_presenter.current_player == AI 
+      render_auto_refresh_board
+    else
+      render_human_board
+    end
   end
 
   def render_auto_refresh_board
